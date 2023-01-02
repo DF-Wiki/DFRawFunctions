330ea9e818c645e442f2b87c1786941493f7bfa1
mexahotabop <mexahotabop@w1l.ru>
2023-01-02 05:47:38 +0300

    rebase extension.json work around new codebase
    fix problem with non evalable wgDFRawPath

--- DFRawFunctions.i18n.magic.php
+++ DFRawFunctions.i18n.magic.php
@@ -4,20 +4,35 @@
  * Dwarf Fortress Raw parser functions
  */
 
-$magicWords = array();
+$magicWords = [];
 
-# English
-$magicWords['en'] = array(
-	'df_raw'			=> array(0, 'df_raw'),
-	'df_tag'			=> array(0, 'df_tag'),
-	'df_tagentry'		=> array(0, 'df_tagentry'),
-	'df_tagvalue'		=> array(0, 'df_tagvalue'),
-	'df_foreachtag'		=> array(0, 'df_foreachtag'),
-	'df_foreachtoken'	=> array(0, 'df_foreachtoken'),
-	'df_makelist'		=> array(0, 'df_makelist'),
-	'df_statedesc'		=> array(0, 'df_statedesc'),
-	'df_cvariation'		=> array(0, 'df_cvariation'),
-	'mreplace'			=> array(0, 'mreplace'),
-	'delay'				=> array(0, 'delay'),
-	'eval'				=> array(0, 'eval'),
-);
+/** English */
+$magicWords['en'] = [
+	'df_raw' => [ 0, 'df_raw' ],
+	'df_tag' => [ 0, 'df_tag' ],
+	'df_tagentry' => [ 0, 'df_tagentry' ],
+    'df_tagvalue' => [ 0, 'df_tagvalue' ],
+    'df_foreachtag' => [ 0, 'df_foreachtag' ],
+    'df_foreachtoken' => [ 0, 'df_foreachtoken' ],
+    'df_makelist' => [ 0, 'df_makelist' ],
+    'df_statedesc' => [ 0, 'df_statedesc' ],
+    'df_cvariation' => [ 0, 'df_cvariation' ],
+    'mreplace' => [ 0, 'mreplace' ],
+    'delay' => [ 0, 'delay' ],
+    'eval' => [ 0, 'eval' ],
+];
+
+$magicWords['ru'] = [
+	'df_raw' => [ 0, 'df_raw' ],
+	'df_tag' => [ 0, 'df_tag' ],
+	'df_tagentry' => [ 0, 'df_tagentry' ],
+    'df_tagvalue' => [ 0, 'df_tagvalue' ],
+    'df_foreachtag' => [ 0, 'df_foreachtag' ],
+    'df_foreachtoken' => [ 0, 'df_foreachtoken' ],
+    'df_makelist' => [ 0, 'df_makelist' ],
+    'df_statedesc' => [ 0, 'df_statedesc' ],
+    'df_cvariation' => [ 0, 'df_cvariation' ],
+    'mreplace' => [ 0, 'mreplace' ],
+    'delay' => [ 0, 'delay' ],
+    'eval' => [ 0, 'eval' ],
+];
